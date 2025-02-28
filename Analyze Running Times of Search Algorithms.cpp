ad26d9b16c067b82a0383ef95b494a2b9ef3aa74
Israr <israr.akh03@gmail.com>
2025-02-27 21:41:59 -0500

    Added N size 100000

--- Analyze Running Times of Search Algorithms.cpp
+++ Analyze Running Times of Search Algorithms.cpp
@@ -56,7 +56,7 @@ int sequentialSearch(const vector<int>& arr, int target) {
 
 int main() {
     //Variables for measuring time
-    int N = 50000;
+    int N = 100000;
     //Variables to accumulate times
     double SumRBS = 0, SumIBS = 0, SumSeqS = 0;
 
